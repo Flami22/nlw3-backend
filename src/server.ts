b496e6abdb1e66d80dccdc35129625cbf220a702
João Henrique <joao.henrique-1998@hotmail.com>
2022-04-06 15:38:20 -0300

    add migrations

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,22 @@
-import express, { request, response } from 'express';
-import path from 'path';
+import express from 'express';
 import cors from 'cors';
+import path from 'path';
 import 'express-async-errors';
-import './database/connection';
+
 import routes from './routes';
-import errorHandler from './errors/handler'
+import errorHandler from './errors/handler';
+
+import './database/connection';
 
 const app = express();
 
-const corsOptions = {
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204,
-    "allowedHeaders": ["Content-Type"]
-  }
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000 
-  
-app.listen(PORT);
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
+
+app.listen(3333, () => {
+  console.log('Server started!');
+});
